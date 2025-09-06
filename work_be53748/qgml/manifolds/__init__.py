bd7f29d40a12bbf1b6241ca5105aaf9852e934a7
Jason Larkin <jasonlarkin84@gmail.com>
2025-09-06 12:00:00 -0400

    Add JAX implementation of QGML matrix trainer

--- work_be53748/qgml/manifolds/__init__.py
+++
@@ -1,5 +0,0 @@
-from .spiral import SpiralManifold
-from .line import LineManifold
-from .sphere import FuzzySphereManifold
-
-__all__ = ['SpiralManifold', 'LineManifold', 'FuzzySphereManifold'] 
--- work_be53748/qgml/quantum/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Quantum computing module for QGML.
-"""
-
-from .matrix_trainer import MatrixConfigurationTrainer, train_matrix_configuration
-
-__all__ = ['MatrixConfigurationTrainer', 'train_matrix_configuration'] 
